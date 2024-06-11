a7cfa6273d1249361fcaa20cd2fe57983df6bf2c
Ganesh Vhanmane <mr.vga1415@gmail.com>
2024-06-11 14:37:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./App.css";
+import WeatherApp from "./Components/WeatherApp/WeatherApp";
+
+function App() {
+ 
+  return (
+    <div>
+     <WeatherApp></WeatherApp>
+    </div>
+  );
+}
+
+export default App;
